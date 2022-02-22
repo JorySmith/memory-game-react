f2030076d11d4e74a50599be2eac43aa698fd452
Jory Smith <46983170+JorySmith@users.noreply.github.com>
2022-02-22 15:33:01 -0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import SingleCard from "./components/SingleCard";
 // Styles
 import './App.css';
 
+// Track if matched, if so, card won't flip back over
 const cardImage = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" }
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false }
 ]
 
 function App() {
@@ -45,13 +46,25 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("IT'S A MATCH!!!")
-        resetTurn()        
-      }
-      resetTurn()      
+        // Return matched cards 'matched' property changed to true
+        // Update previous card state
+        setCards(prevCards => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true} // return updated card obj
+            } else {
+              return card // just return the card ojb as is
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        resetTurn()
+      }            
     }
   }, [choiceOne, choiceTwo])
   
+  console.log(cards)
 
   // After choosing 2 cards, reset cards and increment turns by using prevTurns values
   const resetTurn = () => {
